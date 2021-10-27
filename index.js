7c71959c8fc9b0360a93fdbca84fd9903aaadae1
muzammal kazmi <muzammalkazmi@gmail.com>
2021-10-27 10:50:10 +0500

    connected to datasource via resolver functions

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const resolvers = require('./datasources/resolvers/resolver');
+const LaunchAPI = require('./datasources/launch')
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    dataSources: () => ({
+        launchAPI: new LaunchAPI(),
+
+    })
+});
 server.listen().then(() => {
     console.log(`
       Server is running!
